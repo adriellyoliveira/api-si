0419e2abfb53b46535c82858a414c900fa0c781f
adriellyoliveira <adriellyaraujo826@gmail.com>
2026-09-18 11:16:28 -0300

    feat:  direcionamento de url

--- index.js
+++ index.js
@@ -1,9 +1,72 @@
 import http from "http";
+import { url } from "inspector";
+
+function layout(title, content) {
+	return `
+	<!DOCTYPE html>
+	<html lang= "pt-br">
+	<head>
+		<meta charset= "UTF-8">
+		<title>${title}</title>
+	</head>
+	
+	<body>
+		<nav>
+			<a href="/">Início</a>
+			<a href="/produtos">Produtos</a>
+			<a href="/sobre">Sobre</a>
+		</nav>
+		${content}
+	</body>
+	</html>
+	`
+}
+
+function paginaInicial() {
+	return layout("Inicio", "<h1>Bem-vindx à Loja</h1>")
+}
+
+function paginaProdutos() {
+	return layout("Produtos", "<h1>Página de Produtos</h1>")
+}
+
+function paginaSobre() {
+	return layout("Sobre Nós", "<h1>Sobre Nós</h1>")
+}
+
+function paginaErro() {
+	return layout("Inicio", "<h1>Página não encontrada - 404</h1>")
+}
+
+
 
 const servidor = http.createServer((req, res) => {
 
-	res.writeHead(200, { 'Content-Type': 'text/html' });  
-	res.end("Olá, você");});
+	if (req.url == "/") {
+        res.writeHead(200, { "content-type": "text/html" })
+        res.end(paginaInicial())
+		return
+    }
+
+    else if (req.url == "/produtos") {
+        res.writeHead(200, { "content-type": "text/html" })
+        res.end(paginaProdutos())
+		return
+    }
+
+	else if (req.url == "/sobre") {
+        res.writeHead(200, { "content-type": "text/html" })
+        res.end(paginaSobre())
+		return
+    }
+	
+	else {
+        res.writeHead(404, { "content-type": "text/html" })
+        res.end(paginaErro())
+		return
+    }
+
+})
 	
 servidor.listen(3000, () => {  
 console.log("Servidor rodando em http://localhost:3000");
